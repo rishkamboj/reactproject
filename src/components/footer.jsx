49735f16e29bbf15e8cba14ab99b7f30f538da07
rishkamboj <161414198+rishkamboj@users.noreply.github.com>
2024-02-27 11:15:36 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import call from '../assets/call.png'
 export function Footer(){
      return(<div style={{backgroundColor:"red",display:"flex",justifyContent:"space-evenly",paddingBlockEnd:"30px"}}>
 <p style={{color:"white"}}><span><img src={call}/></span>Toll Free <span>1800 200 1234</span></p>
-<p style={{color:"white"}}><span><img src={fb}/></span>www.facebook.com/cripumps</p>
-<p style={{color:"white"}}><span><img src={www} style={{width:30,height:30}}/></span>www.crigroups.com</p>    
+<p style={{color:"white"}}><span><img src={fb}/></span><span>www.facebook.com/cripumps</span></p>
+<p style={{color:"white"}}><span><img src={www} style={{width:30,height:30}}/></span><span>www.crigroups.com</span></p>    
      </div>)
 }
