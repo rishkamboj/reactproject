0a6f568a1f8b5b3e313952aa3e4b011fcd691d34
rishitkamboj <rishit242@gmail.com>
2024-02-27 11:00:31 +0530

    done

--- src/components/mid.jsx
+++ src/components/mid.jsx
@@ -6,8 +6,8 @@ export function Mid(){
      
      return (
     <div style={{display:"flex",flexDirection:"row", gap:"20px"}}>
-        <div><img src={toffy} style={{height:"510px"}}/></div>
-        <div style={{display:"flex",flexDirection:"column"}}><b><h4 style={{marginBlockEnd:"unset"}}> C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h4></b>
+        <div style={{display:"flex"}}><img src={toffy} style={{height:"510px"}}/></div>
+        <div style={{display:"flex",flexDirection:"column",flexWrap:"wrap"}}><b><h4 style={{marginBlockEnd:"unset"}}> C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h4></b>
         <ul>
           <li> C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
           <li> C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
